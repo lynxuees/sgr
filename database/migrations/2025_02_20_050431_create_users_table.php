60ff13fccc62bef3335a0ddd341b7404520d1cff
Leonardo Montalvo <leonardo.montalvo@khindex.com>
2025-02-25 04:36:08 -0500

    User management complete

--- database/migrations/2025_02_20_050431_create_users_table.php
+++ database/migrations/2025_02_20_050431_create_users_table.php
@@ -15,10 +15,12 @@ return new class extends Migration
             $table->bigIncrements('id');
             $table->string('name', 255);
             $table->string('email', 255)->unique();
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password', 255);
             $table->foreignId('role_id')->constrained('roles')->onDelete('cascade');
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+            $table->softDeletes();
         });
     }
 
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,52 @@
 import './bootstrap';
+import $ from 'jquery';
+import 'jquery-validation';
+import 'datatables.net-dt';
+import { createIcons, icons } from 'lucide';
+
+window.$ = window.jQuery = $;
+
+if (typeof $ !== 'undefined') {
+    console.log("jQuery está funcionando correctamente.");
+} else {
+    console.error("Algo falló: jQuery no está definido.");
+}
+
+$(document).ready(function () {
+    createIcons({ icons });
+    $("#usersTable").DataTable({
+        language: {
+            processing: "Procesando...",
+            search: "Buscar:",
+            lengthMenu: "Mostrar _MENU_ registros por página",
+            info: "Mostrando _START_ a _END_ de _TOTAL_ registros",
+            infoEmpty: "Mostrando 0 a 0 de 0 registros",
+            infoFiltered: "(filtrado de _MAX_ registros en total)",
+            infoPostFix: "",
+            loadingRecords: "Cargando...",
+            zeroRecords: "No se encontraron registros",
+            emptyTable: "No hay datos disponibles en la tabla",
+            aria: {
+                sortAscending: ": Activar para ordenar de forma ascendente",
+                sortDescending: ": Activar para ordenar de forma descendente"
+            },
+            buttons: {
+                copy: "Copiar",
+                colvis: "Visibilidad",
+                csv: "Exportar a CSV",
+                excel: "Exportar a Excel",
+                pdf: "Exportar a PDF",
+                print: "Imprimir"
+            },
+            select: {
+                rows: {
+                    _: "Has seleccionado %d filas",
+                    0: "Haz clic en una fila para seleccionarla",
+                    1: "Has seleccionado 1 fila"
+                }
+            },
+            decimal: ",",
+            thousands: "."
+        }
+    });
+});
